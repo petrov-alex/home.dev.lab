f0888e273983eed62c5687c7f3606fab525ad226
Alex Petrov <petrov.alex.master@yandex.ru>
2024-09-01 22:34:24 +0300

    Test form project added.

---
+++ source/TestForm/Form1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
